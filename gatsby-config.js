7f1bc5039a8037127c09d8ebbb9a00294b1dc0cf
laujamie <36782341+laujamie@users.noreply.github.com>
2019-11-11 13:31:06 -0500

    Update title and copyright information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jamie Lau`,
+    description: `A portfolio website built with Gatsby by Jamie Lau.`,
+    author: `@laujamie`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
