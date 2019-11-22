ff30986f6780adc369d5f29bcd5828f8ea762edc
laujamie <36782341+laujamie@users.noreply.github.com>
2019-11-22 17:50:33 -0500

    Fix template for generating project pages

--- src/projects/sorting-visualizer.md
+++ src/projects/sorting-visualizer.md
@@ -3,6 +3,5 @@ title: "React Sorting Visualizer"
 path: "/projects/react-sorting-visualizer"
 source: "https://github.com/laujamie/react-sorting-visualizer"
 preview: "https://visualizer.jamielau.me"
+description: "A sorting visualizer built in React"
 ---
-
-A sorting visualizer created in React.
--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -1,26 +1,37 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import { Container, Grid, Typography } from "@material-ui/core"
+
+import Layout from "../components/Layout"
+
 const ProjectTemplate = ({ data }) => {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
-    <div>
-      <p>{frontmatter.title}</p>
-    </div>
+    <Layout>
+      <Container>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h1">{frontmatter.title}</Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
   )
 }
 
 export default ProjectTemplate
 
-export const pageQuery = grqphql`
-query($path: String!) {
-    markdownRemark(frontmatter: {path: {eq: $path}}) {
-        frontmatter {
-            path
-            title
-            source
-            preview
-        }
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      frontmatter {
+        path
+        title
+        source
+        preview
+      }
     }
-}`
+  }
+`
