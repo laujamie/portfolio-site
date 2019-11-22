e86fbf60dd0587bbe00c0ce73a82dd54c13e06b6
laujamie <36782341+laujamie@users.noreply.github.com>
2019-11-22 14:17:19 -0500

    Fix invalid path

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `{__dirname}/src/projects`,
+        path: `${__dirname}/src/projects`,
       },
     },
     `gatsby-transformer-sharp`,
