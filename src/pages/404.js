8c7addedb99c35c2e6be1b4baf9095dff082eaa3
laujamie <36782341+laujamie@users.noreply.github.com>
2019-11-22 20:02:04 -0500

    Build out 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,21 @@ import Container from "@material-ui/core/Container"
 import Typography from "@material-ui/core/Typography"
 
 import Layout from "../components/layout"
+import Link from "../components/link"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Container style={{ height: "100vh" }}>
-      <Typography variant="h1" style={{ paddingTop: "20vh" }}>
-        Sorry the page you were looking for wasn't found
+    <Container style={{ height: "60vh" }}>
+      <Typography variant="h2" style={{ paddingTop: "20vh" }}>
+        404: Sorry the page you were looking for wasn't found
+      </Typography>
+      <Typography variant="h4" style={{ paddingTop: "3vh" }}>
+        {" "}
+        <Link to="/" color="inherit">
+          Return home
+        </Link>
       </Typography>
     </Container>
   </Layout>
