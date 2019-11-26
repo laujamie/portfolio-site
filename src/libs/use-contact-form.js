f6c60a822e5a50de3e80d1dd451e30054e078278
laujamie <36782341+laujamie@users.noreply.github.com>
2019-11-25 22:57:46 -0500

    Add new custom hook for contact form

---
+++ src/libs/use-contact-form.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react"
+
+const useContactForm = callback => {
+  const [inputs, setInputs] = useState({})
+
+  const handleSubmit = event => {
+    if (event) {
+      event.preventDefault()
+    }
+    callback()
+  }
+
+  const handleInputChange = event => {
+    event.persist()
+    setInputs(inputs => ({
+      ...inputs,
+      [event.target.name]: event.target.value,
+    }))
+  }
+  return { inputs, handleSubmit, handleInputChange }
+}
+
+export default useContactForm
