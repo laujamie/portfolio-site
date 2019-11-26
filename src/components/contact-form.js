d6873215528096449db99f5d455218059d27e2ff
laujamie <36782341+laujamie@users.noreply.github.com>
2019-11-25 22:58:10 -0500

    Switch contact form to functional component with hooks

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -5,57 +5,54 @@ import Typography from "@material-ui/core/Typography"
 import Container from "@material-ui/core/Container"
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
+import useContactForm from "../libs/use-contact-form"
 
-class ContactForm extends Component {
-  handleSubmit(e) {
-    e.preventDefault()
-  }
+const ContactForm = props => {
+  const { inputs, handleInputChange, handleSubmit } = useContactForm()
 
-  render() {
-    return (
-      <Paper>
-        <Container>
-          <Typography variant="h1">Contact Me</Typography>
-          <form onSubmit={this.handleSubmit}>
-            <Grid container spacing={1}>
-              <Grid item xs={12}>
-                <TextField
-                  id="standard-full-width"
-                  fullWidth
-                  label="Name"
-                  placeholder="John Doe"
-                  InputLabelProps={{ shrink: true }}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  id="standard-full-width"
-                  fullWidth
-                  label="Email"
-                  placeholder="email@example.com"
-                  InputLabelProps={{ shrink: true }}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  id="standard-full-width"
-                  fullWidth
-                  label="Message"
-                  placeholder="Hello World!"
-                  InputLabelProps={{ shrink: true }}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <Button type="Submit" color="primary" variant="contained">
-                  Submit
-                </Button>
-              </Grid>
+  return (
+    <Paper>
+      <Container>
+        <Typography variant="h1">Contact Me</Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <TextField
+                id="standard-full-width"
+                fullWidth
+                label="Name"
+                placeholder="John Doe"
+                InputLabelProps={{ shrink: true }}
+              ></TextField>
             </Grid>
-          </form>
-        </Container>
-      </Paper>
-    )
-  }
+            <Grid item xs={12}>
+              <TextField
+                id="standard-full-width"
+                fullWidth
+                label="Email"
+                placeholder="email@example.com"
+                InputLabelProps={{ shrink: true }}
+              ></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="standard-full-width"
+                fullWidth
+                label="Message"
+                placeholder="Hello World!"
+                InputLabelProps={{ shrink: true }}
+              ></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="Submit" color="primary" variant="contained">
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
+    </Paper>
+  )
 }
 
 export default ContactForm
