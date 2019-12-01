1344cdd2c4afa88e98ab3230ec0aa1153bcd9d7c
laujamie <36782341+laujamie@users.noreply.github.com>
2019-12-01 09:04:15 -0500

    Add POST request to contact form

--- src/libs/use-contact-form.js
+++ src/libs/use-contact-form.js
@@ -3,11 +3,22 @@ import React, { useState } from "react"
 const useContactForm = callback => {
   const [inputs, setInputs] = useState({})
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     if (event) {
       event.preventDefault()
     }
-    console.log(JSON.stringify(inputs))
+    const response = await fetch(
+      "https://uy3ver4rzk.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      }
+    )
+    const myJSON = await response.json()
+    console.log(JSON.stringify(myJSON))
   }
 
   const handleInputChange = event => {
