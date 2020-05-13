40cfc52623e3dd42e19c4a0dfdf45ae9b10f80d9
laujamie <36782341+laujamie@users.noreply.github.com>
2020-05-13 18:17:42 +0000

    Setup netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/admin/config.yml');
   return {
     dir: { input: 'src', output: '_site' },
   };
