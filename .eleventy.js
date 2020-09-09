cceb5f332888c32ee702d1c3875f43ed6f6f732e
laujamie <36782341+laujamie@users.noreply.github.com>
2020-09-09 05:12:31 +0000

    Restructure project directory to be more clear and add API to contact form

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,35 @@
 const moment = require('moment');
+const Terser = require('terser');
+const htmlmin = require('html-minifier');
 
 moment.locale('en');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/static');
   eleventyConfig.addPassthroughCopy('src/admin/config.yml');
+
+  eleventyConfig.addNunjucksAsyncFilter('jsmin', async (code, callback) => {
+    try {
+      const minified = await Terser.minify(code);
+      return callback(null, minified.code);
+    } catch (err) {
+      console.error('Error during terser minify:', err);
+      return callback(err, code);
+    }
+  });
+
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   eleventyConfig.addFilter('dateIso', (date) => {
     return moment(date).toISOString();
@@ -15,6 +40,6 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
-    dir: { input: 'src', output: '_site' },
+    dir: { input: 'src/website', output: '_site' },
   };
 };
