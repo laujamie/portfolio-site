15a0cdc7f621e4386608906d79a2b799d57fa747
laujamie <36782341+laujamie@users.noreply.github.com>
2020-09-09 03:25:35 +0000

    Add util functions to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
-const path = require('path');
+const moment = require('moment');
+
+moment.locale('en');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/static');
   eleventyConfig.addPassthroughCopy('src/admin/config.yml');
+
+  eleventyConfig.addFilter('dateIso', (date) => {
+    return moment(date).toISOString();
+  });
+
+  eleventyConfig.addFilter('dateReadable', (date) => {
+    return moment(date).utc().format('LL');
+  });
+
   return {
     dir: { input: 'src', output: '_site' },
   };
