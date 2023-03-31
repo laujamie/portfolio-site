139e9b2e4d37194065fd998f38fbed8bfca25366
laujamie <36782341+laujamie@users.noreply.github.com>
2023-03-31 13:36:57 -0400

    Add image optimization

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,88 @@
 const moment = require("moment");
 const Terser = require("terser");
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
+const outdent = require("outdent");
 
 moment.locale("en");
 
 now = String(Date.now());
+
+/** Maps a config of attribute-value pairs to an HTML string
+ * representing those same attribute-value pairs.
+ */
+const stringifyAttributes = (attributeMap) => {
+  return Object.entries(attributeMap)
+    .map(([attribute, value]) => {
+      if (typeof value === "undefined") return "";
+      return `${attribute}="${value}"`;
+    })
+    .join(" ");
+};
+
+const imageShortcode = async (
+  src,
+  alt,
+  className = undefined,
+  widths = [400, 800, 1280],
+  formats = ["webp", "jpeg"],
+  sizes = "100vw"
+) => {
+  const imageMetadata = await Image(src, {
+    widths: [...widths, null],
+    formats: [...formats, null],
+    outputDir: "_site/assets/images",
+    urlPath: "/assets/images",
+  });
+
+  const sourceHtmlString = Object.values(imageMetadata)
+    // Map each format to the source HTML markup
+    .map((images) => {
+      // The first entry is representative of all the others
+      // since they each have the same shape
+      const { sourceType } = images[0];
+
+      // Use our util from earlier to make our lives easier
+      const sourceAttributes = stringifyAttributes({
+        type: sourceType,
+        // srcset needs to be a comma-separated attribute
+        srcset: images.map((image) => image.srcset).join(", "),
+        sizes,
+        class: className,
+      });
+
+      // Return one <source> per format
+      return `<source ${sourceAttributes}>`;
+    })
+    .join("\n");
+
+  const getLargestImage = (format) => {
+    const images = imageMetadata[format];
+    return images[images.length - 1];
+  };
+
+  const largestUnoptimizedImg = getLargestImage(formats[0]);
+  const imgAttributes = stringifyAttributes({
+    src: largestUnoptimizedImg.url,
+    width: largestUnoptimizedImg.width,
+    height: largestUnoptimizedImg.height,
+    alt,
+    loading: "lazy",
+    decoding: "async",
+    class: className,
+  });
+  const imgHtmlString = `<img ${imgAttributes}>`;
+
+  const pictureAttributes = stringifyAttributes({
+    class: className,
+  });
+  const picture = `<picture ${pictureAttributes}>
+    ${sourceHtmlString}
+    ${imgHtmlString}
+  </picture>`;
+
+  return outdent`${picture}`;
+};
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
@@ -46,6 +124,8 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addShortcode("version", () => now);
+
+  eleventyConfig.addShortcode("image", imageShortcode);
 
   eleventyConfig.addPassthroughCopy({
     "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
