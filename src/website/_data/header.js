ac3a8e282c772d54e12853b2ae74c30809d9ceca
laujamie <36782341+laujamie@users.noreply.github.com>
2024-06-25 23:52:49 -0400

    refactor + remove padding

---
+++ src/website/_data/header.js
@@ -0,0 +1,24 @@
+module.exports = {
+  items: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "Contact Me",
+      href: "/contact-me",
+    },
+  ],
+};
