be6e1e9538cd851b7bd3e215304aadbd7905dcf5
laujamie <36782341+laujamie@users.noreply.github.com>
2024-06-25 23:52:31 -0400

    update metadata

--- src/website/_data/site.js
+++ src/website/_data/site.js
@@ -6,8 +6,8 @@ module.exports = {
   title: "Jamie Lau",
   baseUrl,
   description:
-    "Jamie Lau is a software engineer, based in Toronto, Canada, working on self-driving trucks at Embark Trucks. " +
-    "Jamie specializes in developing full-stack web applications using Go, React, TypeScript, Python, and C++.",
+    "Jamie Lau is a software engineer, based in Toronto, Canada, working on giving small businesses delivery superpowers at Roundtrip. " +
+    "Jamie specializes in developing full-stack web applications using Go, React, TypeScript, Python, and PostgreSQL.",
   env: process.env.ELEVENTY_ENV === "production",
   linkedin: "https://linkedin.com/in/laujamie5",
   github: "https://github.com/laujamie",
